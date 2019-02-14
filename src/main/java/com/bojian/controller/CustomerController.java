4ffb9e0803a973286979e5c374099c067312d250
liuhuan22 <lh958621@163.com>
2019-02-14 16:25:32 +0800

    修改控制器文件

--- src/main/java/com/bojian/controller/CustomerController.java
+++ src/main/java/com/bojian/controller/CustomerController.java
@@ -1,5 +1,13 @@
 package com.bojian.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class CustomerController {
+	
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
 
 }
